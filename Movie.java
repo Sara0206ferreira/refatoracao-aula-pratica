17f302d10404c98864b9f8f4be7978c7fbd53603
Sara0206ferreira <sfdsilva1@minha.fag.edu.br>
2025-11-26 23:14:42 -0300

    Commit 11
    Criadas as classes Price (abstrata), ChildrensPrice, NewReleasePrice e RegularPrice. Modificada Movie para usar o campo _price (do tipo Price) em vez de _priceCode.

--- Movie.java
+++ Movie.java
@@ -16,7 +16,6 @@ public class Movie {
   public int getPriceCode() {
       return _price.getPriceCode(); 
   }
-
   public void setPriceCode(int arg) { 
       switch (arg) {
          case REGULAR:
@@ -37,9 +36,24 @@ public class Movie {
       return _title;
   }
 
-  public double getCharge(int daysRented) {
-         return _price.getCharge(daysRented);
-   
+  public double getCharge(int daysRented){
+      double result = 0;
+      switch (getPriceCode()) {
+         case REGULAR:
+            result += 2;
+            if (daysRented > 2)
+               result += (daysRented - 2) * 1.5;
+            break;
+         case NEW_RELEASE:
+            result += daysRented * 3;
+            break;
+         case CHILDRENS:
+            result += 1.5;
+            if (daysRented > 3)
+               result += (daysRented - 3) * 1.5;
+             break;
+      }
+      return result;
    }
 
    public int getFrequentRenterPoints(int daysRented){
