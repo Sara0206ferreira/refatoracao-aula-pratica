c5a01d64ae57c44d6eac7737025e813cb99e83ba
Sara0206ferreira <sfdsilva1@minha.fag.edu.br>
2025-11-26 23:33:16 -0300

    Commit 17
    Refatorados os métodos TextStatement.value() e HtmlStatement.value() para se tornarem idênticos, extraindo as partes específicas para métodos auxiliares (headerString, eachRentalString, footerString).

--- TextStatement.java
+++ TextStatement.java
@@ -1,21 +1,28 @@
 import java.util.Enumeration;
 
 public class TextStatement extends Statement {
+   public String headerString(Customer aCustomer) { 
+      return "Rental Record for " + aCustomer.getName() + "\n";
+   }
+     public String eachRentalString(Rental aRental) {
+         return "\t" + aRental.getMovie().getTitle()+ "\t" +
+         String.valueOf(aRental.getCharge()) + "\n";
+      }
+      public String footerString(Customer aCustomer) {
+      return "Amount owed is " + String.valueOf(aCustomer.getTotalCharge()) + 
+             "\n" + "You earned " + 
+             String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
+             " frequent renter points";
+   }
+
    public String value(Customer aCustomer) { 
       Enumeration rentals = aCustomer.getRentals();
-      String result = "Rental Record for " + aCustomer.getName() +
-      "\n";
+      String result = headerString(aCustomer);
       while (rentals.hasMoreElements()) {
          Rental each = (Rental) rentals.nextElement();
-         //show figures for this rental
-         result += "\t" + each.getMovie().getTitle()+ "\t" +
-         String.valueOf(each.getCharge()) + "\n";
+         result += eachRentalString(each); 
       }
-      //add footer lines
-      result += "Amount owed is " +
-      String.valueOf(aCustomer.getTotalCharge()) + "\n";
-      result += "You earned " +
-      String.valueOf(aCustomer.getTotalFrequentRenterPoints()) + " frequent renter points";
+      result += footerString(aCustomer);
       return result;
    }
 }
