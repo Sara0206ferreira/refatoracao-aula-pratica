64a3191a6ec1f143393f3c8e982b72ce334619ad
Sara0206ferreira <sfdsilva1@minha.fag.edu.br>
2025-11-26 23:36:13 -0300

    Commit 18
    Movido o método value() (o Template Method) para a superclasse Statement. Os métodos auxiliares tornam-se abstratos em Statement e são implementados nas subclasses.

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -18,14 +18,4 @@ public class HtmlStatement extends Statement {
              "</EM> frequent renter points<P>";
    }
 
-   public String value(Customer aCustomer) {
-      Enumeration rentals = aCustomer.getRentals();
-      String result = headerString(aCustomer);
-      while (rentals.hasMoreElements()) {
-         Rental each = (Rental) rentals.nextElement();
-         result += eachRentalString(each); 
-      }
-      result += footerString(aCustomer);
-      return result;
    }
-}
--- TextStatement.java
+++ TextStatement.java
@@ -15,14 +15,4 @@ public class TextStatement extends Statement {
              " frequent renter points";
    }
 
-   public String value(Customer aCustomer) { 
-      Enumeration rentals = aCustomer.getRentals();
-      String result = headerString(aCustomer);
-      while (rentals.hasMoreElements()) {
-         Rental each = (Rental) rentals.nextElement();
-         result += eachRentalString(each); 
-      }
-      result += footerString(aCustomer);
-      return result;
    }
-}
